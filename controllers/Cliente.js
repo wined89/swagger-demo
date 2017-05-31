9084541d8985d3c645333a51d816429e2734c4d0
novasolutionsystems <adelgado@novasolutionsystems.com>
2017-05-31 16:57:24 -0500

    subir proyecto

--- controllers/Cliente.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var url = require('url');
-
-var Cliente = require('./ClienteService');
-
-module.exports.clientePOST = function clientePOST (req, res, next) {
-  Cliente.clientePOST(req.swagger.params, res, next);
-};
--- controllers/Contrasea.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var url = require('url');
-
-var Contrasea = require('./ContraseaService');
-
-module.exports.cambioPwd = function cambioPwd (req, res, next) {
-  Contrasea.cambioPwd(req.swagger.params, res, next);
-};
